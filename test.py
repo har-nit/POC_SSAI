e86631fbaa8f8a008b721935fbdcd3a5581e401e
ishanpatle19 <ishan@numeroeins.com>
2021-06-18 10:14:26 +0530

    manifest alter

--- test.py
+++ test.py
@@ -1,26 +1,32 @@
 import ffmpeg_streaming
 from ffmpeg_streaming import Formats, Bitrate, Representation, Size
-
+import subprocess
 
 root = r"C:\Users\admin\PycharmProjects\test2"
-input_file = r"\2.mp4"
-out_file = r"\out.webm"
-out = r"\output_gen_file"
+input_file = r"transform\merged.m3u8"
+out_file = r"transform\final_video.mp4"
 video = ffmpeg_streaming.input(root+input_file)
 
 
-def hls_enc():
-    #A path you want to save a random key to your local machine
-    save_to = root+'/key'
+# def hls_enc():
+#     #A path you want to save a random key to your local machine
+#     save_to = root+'/key'
+#
+#     #A URL (or a path) to access the key on your website
+#     url = 'http://www.localhost.com/test2/key'
+#     # or url = '/"PATH TO THE KEY DIRECTORY"/key';
+#
+#     hls = video.hls(Formats.h264())
+#     hls.encryption(save_to, url)
+#     hls.auto_generate_representations()
+#     hls.output(root+'/hls.m3u8')
 
-    #A URL (or a path) to access the key on your website
-    url = 'http://www.localhost.com/test2/key'
-    # or url = '/"PATH TO THE KEY DIRECTORY"/key';
 
-    hls = video.hls(Formats.h264())
-    hls.encryption(save_to, url)
-    hls.auto_generate_representations()
-    hls.output(root+'/hls.m3u8')
+# hls_enc()
 
 
-hls_enc()
+def convert_mp4():
+    command = 'ffmpeg -i ' + input_file + ' ' + out_file
+    subprocess.run(command)
+
+convert_mp4()
